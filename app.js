632f67121fa0e0831be38aa0d014007af124b74a
Eranio <63237713+Eranio-bit@users.noreply.github.com>
2025-11-19 17:33:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Your GitHub raw image URL
-const githubImageURL = "https://github.com/Eranio-bit/WeddingGift/blob/6696db5da1bc8080fad24fbda7555ae7e2f933d9/image.png";
+const githubImageURL = "https://github.com/Eranio-bit/WeddingGift/blob/6696db5da1bc8080fad24fbda7555ae7e2f933d9/image.png?raw=true";
 
 // Your text
 const text = "Hello! This image is loaded from GitHub.";
