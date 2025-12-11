1d1b872c2e684940cc1965e927acac3450ffb3fd
Eranio-bit <63237713+Eranio-bit@users.noreply.github.com>
2025-12-11 09:21:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const githubImageURL = "https://github.com/Eranio-bit/WeddingGift/blob/6696db5da1bc8080fad24fbda7555ae7e2f933d9/image.png?raw=true";
 
 // Your text
-const text = "Hello! Contribute here";
+const text = "Hello! Please contribute here";
 
 document.getElementById("title").textContent = text;
 document.getElementById("image").src = githubImageURL;
